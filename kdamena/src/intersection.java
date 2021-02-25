a82c6e295d029774ffad81dd362d65b276654480
djihaaane <djihane.harouni@gmail.com>
2021-02-25 19:13:55 +0100

    street+inter

---
+++ kdamena/src/intersection.java
@@ -0,0 +1,7 @@
+public class intersection {
+    int id_Intersection;
+    street[] street_Coming_In;
+    street[] street_coming_Out;
+    
+    
+}
---
+++ kdamena/src/street.java
@@ -0,0 +1,6 @@
+public class street{
+char street_Name ;
+boolean intersection_S ;
+boolean intersection_E;
+int car_Take_Time;
+}
